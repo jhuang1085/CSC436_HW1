557844e287d0368f5d6248ed316757137f95081f
jhuang1085 <huangjingyi85@qq.com>
2020-10-20 16:35:38 -0500

    homework2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { TicketComponent } from './ticket/ticket.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TicketComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/ticket/ticket.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+export class Ticket {
+  public deptname: string;
+  public arrivename: string;
+  public regdate: Date;
+  public numpassage: number;
+}
+@Component({
+  selector: 'app-ticket',
+  templateUrl: './ticket.component.html',
+  styleUrls: ['./ticket.component.css']
+})
+
+export class TicketComponent implements OnInit {
+  constructor() { }
+
+  model = new Ticket();
+  Airport: string[] = [
+    'SFO',
+    'LAX',
+    'WA',
+    'SEA',
+    'ORD',
+    'MLA',
+    'BOS'
+  ];
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(form): void {
+    console.log(form.value);
+  }
+}
